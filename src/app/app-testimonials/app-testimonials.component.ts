6b47fd4d87828c34ba0fad06c3de362c01ef5d9a
solvemate2018 <35694266+solvemate2018@users.noreply.github.com>
2023-11-09 13:50:10 +0100

    Added condition for controlls on testimonials

--- src/app/app-testimonials/app-testimonials.component.ts
+++ src/app/app-testimonials/app-testimonials.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { DataService } from '../data.service';
 import { Testimonial } from '../interfaces/Testimonial';
 
@@ -9,10 +9,29 @@ import { Testimonial } from '../interfaces/Testimonial';
 })
 export class AppTestimonialsComponent {
   testimonials: Testimonial[] = [];
+  showControls: boolean = false;
 
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.testimonials = this.dataService.getTestimonials();
   }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+      // Update screen size on window resize
+      this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    const width = window.innerWidth;
+
+    if (width < 768) {
+        this.showControls = false;
+    } else if (width >= 768 && width < 1024) {
+        this.showControls = false;
+    } else {
+        this.showControls = true;
+    }
+}
 }
